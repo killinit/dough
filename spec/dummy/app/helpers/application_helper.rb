86c5417ee676cdc6f8a62d34c9520b7bbc59b10f
Phil Lee <asmega@ph-lee.com>
2014-07-25 11:15:16 +0100

    add css styleguide

--- spec/dummy/app/helpers/application_helper.rb
+++ spec/dummy/app/helpers/application_helper.rb
@@ -19,6 +19,34 @@ module ApplicationHelper
     format_tokens_as_html(tokens, line_numbers)
   end
 
+  def css(line_numbers: false, &block)
+    source = strip_leading_indentation_from_source(capture(&block))
+    tokens = Rouge::Lexers::CSS.lex(source)
+
+    format_tokens_as_html(tokens, line_numbers)
+  end
+
+  def yaml(line_numbers: false, &block)
+    source = strip_leading_indentation_from_source(capture(&block))
+    tokens = Rouge::Lexers::YAML.lex(source)
+
+    format_tokens_as_html(tokens, line_numbers)
+  end
+
+  def Terminal256(line_numbers: false, &block)
+    source = strip_leading_indentation_from_source(capture(&block))
+    tokens = Rouge::Lexers::HTML.lex(source)
+
+    format_tokens_as_html(tokens, line_numbers)
+  end
+
+  def scss(line_numbers: false, &block)
+    source = strip_leading_indentation_from_source(capture(&block))
+    tokens = Rouge::Lexers::Scss.lex(source)
+
+    format_tokens_as_html(tokens, line_numbers)
+  end
+
   def strip_leading_indentation_from_source(source)
     source.strip.gsub(/^[  ]{2}/, '')
   end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -5,6 +5,7 @@ Dummy::Application.routes.draw do
   resources :forms, only: [:index]
   resources :typography, only: [:index]
   resources :html, only: [:index]
+  resources :css, only: [:index]
 
   get "/integrated_dough_helper", to: "integrated_dough_helper#index"
 end
